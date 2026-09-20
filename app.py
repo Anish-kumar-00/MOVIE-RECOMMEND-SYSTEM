a5a2f1ecc5895f1ffa7cd91cfaa8c4404864d731
Anish Kumar <ay6704197@gmail.com>
2026-09-20 19:21:11 +0530

    update app.py

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ if st.button("🚀 Show Recommendation"):
         "✨ Recommended Movies"
     )
 
-    cols = st.columns(6)
+    cols = st.columns(10)
 
     for col, name, poster, movie_id in zip(
         cols,
